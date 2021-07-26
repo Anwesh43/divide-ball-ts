bbe3f7f3f79add5537ffdb213789711c131c3e2e
Anwesh43 <anweshthecool0@gmail.com>
2021-07-26 20:35:28 +0530

    adding DBNode class

--- index.ts
+++ index.ts
@@ -131,3 +131,45 @@ class Animator {
         }
     }
 }
+
+class DBNode {
+
+    prev : DBNode 
+    next : DBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DBNode {
+        var curr : DBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
